4fcc86bfabb7f0e76c69768d102e52ffcac4947a
Hanif <marbling1293@naver.com>
2020-11-24 22:02:11 +0900

    [ADD] File Download Service

--- src/main/java/com/hackerton/propofol/service/PostService.java
+++ src/main/java/com/hackerton/propofol/service/PostService.java
@@ -3,12 +3,15 @@ package com.hackerton.propofol.service;
 import com.hackerton.propofol.dto.PostContentResponse;
 import com.hackerton.propofol.dto.PostListResponse;
 import com.hackerton.propofol.dto.PostWriteRequest;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public interface PostService {
 
     void write(PostWriteRequest postWriteRequest);
     PostListResponse getList(Pageable pageable);
     PostContentResponse getContent(Long postId);
+    ResponseEntity<Resource> downloadFile(Long postId, String fileId);
     void deletePost(Long postId);
 }
--- src/main/java/com/hackerton/propofol/service/PostServiceImpl.java
+++ src/main/java/com/hackerton/propofol/service/PostServiceImpl.java
@@ -1,8 +1,10 @@
 package com.hackerton.propofol.service;
 
 import com.hackerton.propofol.domain.Post;
+import com.hackerton.propofol.domain.PostFile;
 import com.hackerton.propofol.domain.User;
 import com.hackerton.propofol.domain.repository.CommentRepository;
+import com.hackerton.propofol.domain.repository.PostFileRepository;
 import com.hackerton.propofol.domain.repository.PostRepository;
 import com.hackerton.propofol.domain.repository.UserRepository;
 import com.hackerton.propofol.dto.PostContentResponse;
@@ -15,11 +17,17 @@ import com.hackerton.propofol.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,6 +42,7 @@ public class PostServiceImpl implements PostService {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final PostFileRepository postFileRepository;
 
     private final AuthenticationFacade authenticationFacade;
 
@@ -66,9 +75,16 @@ public class PostServiceImpl implements PostService {
 
             String path = Paths.get(fileDirPath, fileId).toString();
 
-            postRepository.save(post.updateFile(fileId, postWriteRequest.getFile().getOriginalFilename()));
+            PostFile postFile = postFileRepository.save(
+                    PostFile.builder()
+                            .postId(post.getId())
+                            .fileId(fileId)
+                            .fileName(postWriteRequest.getFile().getOriginalFilename())
+                            .filePath(path)
+                            .build()
+            );
 
-            postWriteRequest.getFile().transferTo(new File(path));
+            postWriteRequest.getFile().transferTo(new File(postFile.getFilePath()));
         }
     }
 
@@ -113,6 +129,8 @@ public class PostServiceImpl implements PostService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(PostNotFoundException::new);
 
+        PostFile postFile = postFileRepository.findByPostId(postId);
+
         User writer = userRepository.findById(post.getUserId())
                 .orElseThrow(UserNotFoundException::new);
 
@@ -124,9 +142,27 @@ public class PostServiceImpl implements PostService {
                 .image(writer.getImage())
                 .createdAt(post.getCreatedAt())
                 .isMine(writer.equals(user))
-                .fileId(post.getFileId())
-                .fileName(post.getFileName())
+                .fileId(postFile.getFileId())
+                .fileName(postFile.getFileName())
                 .build();
+    }
+
+    @SneakyThrows
+    @Override
+    public ResponseEntity<Resource> downloadFile(Long postId, String fileId) {
+        postRepository.findById(postId)
+                .orElseThrow(PostNotFoundException::new);
+
+        PostFile postFile = postFileRepository.findByPostId(postId);
+
+        Resource resource = loadFileAsResource(postFile.getFilePath());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, String.format(
+                        "attachment; filename=\"%s\"", postFile.getFileName()
+                ))
+                .body(resource);
     }
 
     @Override
@@ -138,5 +174,19 @@ public class PostServiceImpl implements PostService {
                 .orElseThrow(PostNotFoundException::new);
 
         postRepository.deleteById(postId);
+    }
+
+    private Resource loadFileAsResource(String fileName) throws FileNotFoundException {
+        try {
+            Path filePath = new File(fileName).toPath();
+            Resource resource = new UrlResource(filePath.toUri());
+            if(resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException();
+            }
+        } catch (Exception ex) {
+            throw new FileNotFoundException();
+        }
     }
 }
