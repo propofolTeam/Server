acd74446ffc06ca795cbb63ad2bf453e4a752370
Hanif <marbling1293@naver.com>
2020-11-24 15:30:06 +0900

    [FIX] profile Response

--- src/main/java/com/hackerton/propofol/service/UserService.java
+++ src/main/java/com/hackerton/propofol/service/UserService.java
@@ -5,9 +5,11 @@ import com.hackerton.propofol.dto.LoginRequest;
 import com.hackerton.propofol.dto.ProfileResponse;
 import com.hackerton.propofol.dto.TokenResponse;
 
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
 
     void join(JoinRequest joinRequest);
     TokenResponse login(LoginRequest loginRequest);
-    ProfileResponse getProfile(Long userId);
+    ProfileResponse getProfile(Long userId, Pageable pageable);
 }
