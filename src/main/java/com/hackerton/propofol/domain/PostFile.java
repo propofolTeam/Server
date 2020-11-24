12a1e2115ad07086e7a637f9ed8ccdf17ca71476
Hanif <marbling1293@naver.com>
2020-11-24 22:01:44 +0900

    [ADD] PostFile entity

---
+++ src/main/java/com/hackerton/propofol/domain/PostFile.java
@@ -0,0 +1,29 @@
+package com.hackerton.propofol.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostFile {
+
+    @Id
+    private String fileId;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Post post;
+}
