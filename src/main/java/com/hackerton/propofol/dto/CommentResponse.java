5b27db00bec6e625e12e73054a2223d691e34a29
Hanif <marbling1293@naver.com>
2020-11-24 22:25:11 +0900

    [ADD] Comment Response

---
+++ src/main/java/com/hackerton/propofol/dto/CommentResponse.java
@@ -0,0 +1,19 @@
+package com.hackerton.propofol.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CommentResponse {
+
+    private Long id;
+
+    private String comment;
+
+    private String writer;
+
+    private Long userId;
+
+    private String image;
+}
