127887bd92f440e072ed67ac7afa5b27f2da500b
Hanif <marbling1293@naver.com>
2020-11-24 11:53:57 +0900

    [ADD] Custom UserDetails

---
+++ src/main/java/com/hackerton/propofol/security/AuthDetailsService.java
@@ -0,0 +1,25 @@
+package com.hackerton.propofol.security;
+
+import com.hackerton.propofol.domain.User;
+import com.hackerton.propofol.domain.repository.UserRepository;
+import com.hackerton.propofol.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
