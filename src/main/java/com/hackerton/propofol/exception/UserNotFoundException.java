e32b33779bcc70167275b3e400ac26534e3894ab
Hanif <marbling1293@naver.com>
2020-11-24 11:56:06 +0900

    [ADD] UserNotFound

---
+++ src/main/java/com/hackerton/propofol/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.hackerton.propofol.exception;
+
+public class UserNotFoundException extends BusinessException {
+
+    public UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}
