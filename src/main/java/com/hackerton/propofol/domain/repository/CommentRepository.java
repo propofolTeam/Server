e50d4ea7cbf52260e74d7ab8b5754853bf934008
Hanif <marbling1293@naver.com>
2020-11-24 22:25:00 +0900

    [ADD] findByPostId

--- src/main/java/com/hackerton/propofol/domain/repository/CommentRepository.java
+++ src/main/java/com/hackerton/propofol/domain/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.hackerton.propofol.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Integer countByPostId(Long postId);
+    List<Comment> findByPostId(Long postId);
 }
