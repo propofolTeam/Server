41e85f5f8ba3c936f22a46ba8b7660cea9f1380f
Hanif <marbling1293@naver.com>
2020-11-24 11:54:46 +0900

    [ADD] Login Controller

--- src/main/java/com/hackerton/propofol/controller/UserController.java
+++ src/main/java/com/hackerton/propofol/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.hackerton.propofol.controller;
 
 import com.hackerton.propofol.dto.JoinRequest;
+import com.hackerton.propofol.dto.LoginRequest;
+import com.hackerton.propofol.dto.TokenResponse;
 import com.hackerton.propofol.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,5 +19,10 @@ public class UserController {
     @PostMapping("/join")
     public void join(@ModelAttribute @Valid JoinRequest joinRequest) {
         userService.join(joinRequest);
+    }
+
+    @PostMapping("/login")
+    private TokenResponse login(@RequestBody @Valid LoginRequest loginRequest) {
+        return userService.login(loginRequest);
     }
 }
