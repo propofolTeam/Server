79e3bc44d2a2a81a2334989c872c30acf6d9a69a
Hanif <marbling1293@naver.com>
2020-11-24 10:43:10 +0900

    [ADD] Init

---
+++ src/main/java/com/hackerton/propofol/PropofolApplication.java
@@ -0,0 +1,13 @@
+package com.hackerton.propofol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropofolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropofolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackerton/propofol/PropofolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerton.propofol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropofolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
