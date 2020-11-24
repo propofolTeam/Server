ce1f9803cede98173db76aecc8d904bf0bbbc03c
Hanif <marbling1293@naver.com>
2020-11-24 13:51:43 +0900

    [ADD] profile Response

---
+++ src/main/java/com/hackerton/propofol/dto/ProfileResponse.java
@@ -0,0 +1,19 @@
+package com.hackerton.propofol.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProfileResponse {
+
+    private String email;
+
+    private String name;
+
+    private String image;
+
+    private boolean isMine;
+
+    private String[] posts;
+}
