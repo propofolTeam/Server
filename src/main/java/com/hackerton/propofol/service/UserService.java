796f8d4ac322e081907b9f3b240f6e3e19498e55
Hanif <marbling1293@naver.com>
2020-11-24 11:55:25 +0900

    [ADD] Login Service

--- src/main/java/com/hackerton/propofol/service/UserService.java
+++ src/main/java/com/hackerton/propofol/service/UserService.java
@@ -1,8 +1,11 @@
 package com.hackerton.propofol.service;
 
 import com.hackerton.propofol.dto.JoinRequest;
+import com.hackerton.propofol.dto.LoginRequest;
+import com.hackerton.propofol.dto.TokenResponse;
 
 public interface UserService {
 
     void join(JoinRequest joinRequest);
+    TokenResponse login(LoginRequest loginRequest);
 }
